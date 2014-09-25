d982350e30842938757ae9993978c33b0569a98e
pkhanal <prashantkhanal@gmail.com>
2014-09-24 21:34:59 -0700

    fixed an issue where we were incorrectly checking whether or not Kaazing.namespace function exists

--- src/Namespace.js
+++ src/Namespace.js
@@ -8,19 +8,18 @@
 // Root Namespace Object
 var Kaazing = Kaazing || {};
 
-if (Kaazing.namespace !== "function") {
+if (typeof Kaazing.namespace !== "function") {
     // The implementation is nondestructive i.e. if a namespace exists, it won't be created.
 	Kaazing.namespace = function(namespace_string) {
-	    var parts = namespace_string.split('.'), 
-	    parent = Kaazing, 
-	    i;
+	    var parts = namespace_string.split('.');
+	    var parent = Kaazing;
 	    
 	    // strip redundant leading global
-	    if (parts[0] === Kaazing) {
+	    if (parts[0] === "Kaazing") {
 	        parts = parts.slice(1);
 	    }
 	    
-	    for (i = 0; i < parts.length; i++) {
+	    for (var i = 0; i < parts.length; i++) {
 	        // create a property if it does not exist
 	        if (typeof parent[parts[i]] === "undefined") {
 	            parent[parts[i]] = {};
